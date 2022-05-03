b94609b4188a6562b1d159a4b44b6da4188baf88
Max Sichel <msik314@gmail.com>
2022-05-03 00:17:02 -0400

    fix linalloc to work in a c++ source file

--- util/linalloc.h
+++ util/linalloc.h
@@ -3,10 +3,21 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#else
+#include <stdbool.h>
+#endif //__cplusplus
+
 void linInit(void* mem, uint32_t size);
 void* linalign(uint32_t size, uint32_t alignment);
 #define linalloc(size) linalign(size, 16)
 void linReset();
 void* linCleanup();
+
+#ifdef __cplusplus
+};
+#endif //__cplusplus
 
 #endif //LINALLOC
